686788b9f6e876689e112f7eb70c0387bc724f11
Danny Nguyen <dannynguyen626@yahoo.com>
2020-08-05 00:58:26 -0700

    full portfolio

--- bootstrap_app/urls.py
+++ bootstrap_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.welcome),
+    path('', views.index),
+    path('timeline', views.timeline),
     path('about_me', views.about)		   
 ]
--- bootstrap_app/views.py
+++ bootstrap_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, HttpResponse
 
 
+def index(request):
+    return render(request, 'index.html')
+
+
 def timeline(request):
     return render(request, 'timeline.html')
 
