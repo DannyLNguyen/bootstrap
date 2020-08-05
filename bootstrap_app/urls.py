ae3a54d4bb20e38bbbbc1c4127f193aa41b83309
Danny Nguyen <dannynguyen626@yahoo.com>
2020-08-04 23:47:14 -0700

    bootstrap timeline and bio
    learning bootstrap and applying it to current portfolio page

--- bootstrap_app/urls.py
+++ bootstrap_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path     
 from . import views
 urlpatterns = [
-    path('', views.welcome),	   
+    path('', views.welcome),
+    path('about_me', views.about)		   
 ]
--- bootstrap_app/views.py
+++ bootstrap_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
 
-def welcome(request):
-    return render(request, 'welcome.html')
+def timeline(request):
+    return render(request, 'timeline.html')
+
+def about(request):
+        return render(request, 'about_me.html')
